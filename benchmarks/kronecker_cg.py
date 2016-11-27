e6d4f3fc9e50cc87c76e3fd952ce4ece7376ed65
Vladimir Feinberg <vyf@princeton.edu>
2016-11-27 10:17:05 -0500

    minres testing, ill conditioned

--- benchmarks/kronecker_cg.py
+++ benchmarks/kronecker_cg.py
@@ -29,9 +29,10 @@ def stress_kronecker_solve(top, d):
     assert np.linalg.matrix_rank(M) == n * d
 
     tol = 1e-6
+    cond = np.linalg.cond(M)
 
     print('    size {}x{} tol {:g} cond {}'
-          .format(n, d, tol, np.linalg.cond(M)))
+          .format(n, d, tol, cond))
     ctr = 0
     def inc(_):
         nonlocal ctr
@@ -39,21 +40,42 @@ def stress_kronecker_solve(top, d):
     with contexttimer.Timer() as cg_sparse_time:
         cg_sparse, success = scipy.sparse.linalg.cg(
             A, b, tol=tol, callback=inc, maxiter=(n*d))
+        if cond < 1/tol:
+            assert success == 0
     print('    Sparse Kron CG {:4.4f} s ({} iterations)'
           .format(cg_sparse_time.elapsed, ctr))
+    if ctr == n * d:
+        print('      ** No convergence: residual {}'.format(
+            np.linalg.norm(A.matvec(cg_sparse) - b)))
 
-    with contexttimer.Timer() as cg_dense_time:
-        cg_dense, success = scipy.sparse.linalg.cg(M, b, tol=tol)
-        assert success == 0
+    ctr = 0
+    with contexttimer.Timer() as mr_sparse_time:
+        mr_sparse, success = scipy.sparse.linalg.minres(
+            A, b, tol=tol, callback=inc, maxiter=(n*d))
+        if cond < 1/tol:
+            assert success == 0
+    print('    Minres         {:4.4f} s ({} iterations)'
+          .format(mr_sparse_time.elapsed, ctr))
+    if ctr == n * d:
+        print('      ** No convergence: residual {}'.format(
+            np.linalg.norm(A.matvec(mr_sparse) - b)))
+
+    if cond >= 1/tol:
+        print('    Skipping Dense CG (too ill-conditioned)')
+    else:
+        with contexttimer.Timer() as cg_dense_time:
+            cg_dense, success = scipy.sparse.linalg.cg(M, b, tol=tol)
+            assert success == 0
         print('    Dense CG       {:4.4f} s'.format(cg_dense_time.elapsed))
 
     with contexttimer.Timer() as linsolve_time:
         linsolve = np.linalg.solve(M, b)
     print('    Linear solve   {:4.4f} s'.format(linsolve_time.elapsed))
 
-    np.testing.assert_allclose(cg_sparse, linsolve, atol=(tol * 2))
-    np.testing.assert_allclose(cg_sparse, cg_dense, atol=(tol * 2))
-
+    if cond < 1/tol:
+        np.testing.assert_allclose(cg_sparse, linsolve, atol=(tol * 2))
+        np.testing.assert_allclose(mr_sparse, linsolve, atol=(tol * 2))
+        np.testing.assert_allclose(cg_dense, linsolve, atol=(tol * 2))
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -81,8 +103,15 @@ if __name__ == "__main__":
     stress_kronecker_solve(top, d)
 
     # Poorly-conditioned
-    print('skipping poorly-conditioned case:')
-    print('    it is unrealistic and breaks CG')
+    print('linear decrease (poor-cond)')
+    up = np.arange(n // 8) + 1
+    down = np.copy(up[::-1])
+    up = up / 2
+    top = np.zeros(n)
+    updown = np.add.accumulate(np.hstack([up, down]))[::-1]
+    top[:len(updown)] = updown
+    stress_kronecker_solve(top, d)
+
 
     print('exponentially decreasing (realistic)')
     stress_kronecker_solve(np.exp(-np.arange(n)), d)
