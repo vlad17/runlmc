813ea0fc0159e6f291d40df78c604666810a8386
Vladimir Feinberg <vyf@princeton.edu>
2016-11-30 16:00:27 -0500

    extra space

--- docbuild.sh
+++ docbuild.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sphinx-apidoc --separate --force --output-dir=doc/_generated runlmc/ 
+sphinx-apidoc --separate --force --output-dir=doc/_generated runlmc/
 PYTHONPATH=. sphinx-build -j $(nproc) -c doc/ -b html doc/_generated doc/_generated/_build/
