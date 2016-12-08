62d63ea91867cbfeed17c37ae85c19e74a14e7ca
Vladimir Feinberg <vyf@princeton.edu>
2016-12-08 16:14:54 -0500

    Exactness parameter for eig.py benchmark

--- benchmarks/eig.py
+++ benchmarks/eig.py
@@ -12,7 +12,7 @@ from runlmc.linalg.toeplitz import Toeplitz
 from runlmc.linalg.kronecker import Kronecker
 import runlmc.util.testing_utils as utils
 
-def stress_kronecker_eig(top, d):
+def stress_kronecker_eig(top, d, exact):
     n = len(top)
     b = np.random.rand(n * d)
     dense = utils.rand_psd(d)
@@ -24,11 +24,12 @@ def stress_kronecker_eig(top, d):
     assert np.linalg.matrix_rank(toep) == n
     assert np.linalg.matrix_rank(M) == n * d
 
+    exact = bool(exact)
     cutoff = 1e-4
     cond = np.linalg.cond(M)
     dom = np.count_nonzero(top[top > top.max() * cutoff])
-    print('    dominant entries {} size {}x{} cutoff {:g} cond {}'
-          .format(dom, n, d, cutoff, cond))
+    print('    dominant entries {} size {}x{} cutoff {:g} cond {} exact {}'
+          .format(dom, n, d, cutoff, cond, exact))
 
     with contexttimer.Timer() as solve_time:
         eigs = np.linalg.eigvalsh(M)
@@ -39,7 +40,7 @@ def stress_kronecker_eig(top, d):
         solve_time.elapsed))
 
     with contexttimer.Timer() as solve_time:
-        my_eigs = A.eig(cutoff)
+        my_eigs = A.eig(cutoff, exact)
     print('    {} sec {:8.4f}'.format(
         'kron decomp'.rjust(20),
         solve_time.elapsed))
@@ -52,11 +53,12 @@ def stress_kronecker_eig(top, d):
         np.abs(eigs[:minlen] - my_eigs[:minlen]).mean()))
 
 if __name__ == "__main__":
-    if len(sys.argv) not in [3, 4]:
-        print('Usage: python eig.py n d [seed]')
+    if len(sys.argv) not in [4, 5]:
+        print('Usage: python eig.py n d exact [seed]')
         print()
         print('n > 8 is the size of the Toeplitz submatrix')
         print('d > 0 is the size of the dense submatrix')
+        print('exact is a binary integer')
         print('default seed is 1234')
         print('this eigendecomposes the kronecker product system size n * d')
         print('choose d == 1 and n large to test Toeplitz eig only')
@@ -64,7 +66,8 @@ if __name__ == "__main__":
 
     n = int(sys.argv[1])
     d = int(sys.argv[2])
-    seed = int(sys.argv[3]) if len(sys.argv) > 3 else 1234
+    exact = int(sys.argv[3])
+    seed = int(sys.argv[4]) if len(sys.argv) > 4 else 1234
 
     assert n > 8
     assert d > 0
@@ -73,10 +76,10 @@ if __name__ == "__main__":
     print('* = no convergence')
 
     print('random (well-cond) ')
-    stress_kronecker_eig(utils.random_toep(n), d)
+    stress_kronecker_eig(utils.random_toep(n), d, exact)
 
     print('linear decrease (poor-cond)')
-    stress_kronecker_eig(utils.poor_cond_toep(n), d)
+    stress_kronecker_eig(utils.poor_cond_toep(n), d, exact)
 
     print('exponentially decreasing (realistic)')
-    stress_kronecker_eig(utils.exp_decr_toep(n), d)
+    stress_kronecker_eig(utils.exp_decr_toep(n), d, exact)
