4a4b21c6db2ceb09c83be633516aacfcd10da974
Vladimir Feinberg <vyf@princeton.edu>
2017-01-01 10:44:50 -0800

    Added some (inactive) alternative apprx eigenvalues

--- runlmc/linalg/sum_matrix.py
+++ runlmc/linalg/sum_matrix.py
@@ -61,19 +61,73 @@ class SumMatrix(PSDMatrix):
         For details, and an elementary proof of the solution, see
         [TODO(PAPER) section .].
         """
-        # separate into a minimum constant diagonal perturbation
-        # plus a sum of matrices
-        Q = len(self.Ks)
-        n = self.shape[0]
-        eigs = np.zeros((Q, n)) + min_eig
-        for i, K in enumerate(self.Ks):
-            eigs_K = K.eig(min_eig, exact=False)
-            eigs[i, :len(eigs_K)] = eigs_K
 
-        eigsT = eigs.T
-        for i in range(Q):
-            eigsT = eigsT[np.argsort(eigsT[:, :i].sum(-1))]
-            eigsT[:, i][::-1].sort()
-        eigs = eigsT.T
+        algorithm = 'greedy_fiedler'
+        if algorithm == 'greedy_fiedler':
+            # separate into a minimum constant diagonal perturbation
+            # plus a sum of matrices
+            Q = len(self.Ks)
+            n = self.shape[0]
+            eigs = np.zeros((Q, n)) + min_eig
+            for i, K in enumerate(self.Ks):
+                eigs_K = K.eig(min_eig, exact=False)
+                eigs[i, :len(eigs_K)] = eigs_K
 
-        return eigs.sum(axis=0)
+            eigsT = eigs.T
+            for i in range(Q):
+                eigsT = eigsT[np.argsort(eigsT[:, :i].sum(-1))]
+                eigsT[:, i][::-1].sort()
+                eigs = eigsT.T
+
+            return eigs.sum(axis=0)
+
+        elif algorithm == 'simultaneous_diag':
+            # LOWER BOUND
+            # useful for kernels which have same eigenvectors
+            # Potential idea:
+            # Partition kernels into subsets which have the same
+            # eigendecomposition (perhaps if the class that generates them
+            # is the same). Within a partition, we add eigenvalues
+            # with 'simulatneous_diag'. Between partitions, we use
+            # 'greedy_fiedler'.
+            eigs = np.zeros(self.shape[0])
+            for K in self.Ks:
+                eigs_K = K.eig(min_eig, exact=False)
+                eigs[:len(eigs_K)] += eigs_K
+
+            eigs[eigs == 0] = min_eig
+            return eigs
+        else: # Greedy Weyl (has guarantee)
+            # can either greedily max a lower bound
+            # or greedily minimize an upper bound
+
+            option = 'minimize_ub' # or 'maximize_lb'
+            chooser = np.argmin if option == 'minimize_ub' else np.argmax
+            reverse = np.flipud if option == 'maximize_lb' else lambda x: x
+
+            n = self.shape[0]
+            s = max(int(np.log(n)), 5) # s=n/2 is exact weyl
+            eigs = np.zeros(self.shape[0])
+            eigs_K = np.zeros_like(eigs)
+            for K in self.Ks:
+                prev_eigs = reverse(np.copy(eigs))
+                eigs_small = K.eig(min_eig, exact=False)
+                eigs_K[:] = min_eig
+                eigs_K[:len(eigs_small)] = eigs_small
+                i, j = 0, 0
+                for k in range(n):
+                    lowest_i = max(i - s, 0)
+                    highest_i = min(s + (s - (i - lowest_i)), n - 1, k)
+                    # Weyl says that
+                    # the k-th largest eigenvalue is less than the sum
+                    # of the i-th largest of the first matrix and
+                    # the (k - i)-th largest of the second.
+                    i_candidates = np.arange(lowest_i, highest_i + 1)
+                    j_candidates = k - i_candidates
+                    vals = prev_eigs[i_candidates] + eigs_K[j_candidates]
+                    best = chooser(vals)
+                    eigs[k] = vals[best]
+                    i, j = i_candidates[best], j_candidates[best]
+
+            eigs[eigs < min_eig] = min_eig
+            return eigs
