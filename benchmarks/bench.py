656a43a0627be656ad76e2a611c3601fc1d041b7
Vladimir Feinberg <vyf@princeton.edu>
2017-01-31 13:37:48 -0500

    Removed logdet test

--- benchmarks/bench.py
+++ benchmarks/bench.py
@@ -33,7 +33,7 @@ eps > 0 is the constant diagonal perturbation mean (a float)
 kern is the kernel type, default rbf, one of 'rbf' 'periodic' 'matern' 'mix'
 seed is the random seed, default 1234
 test-type performs a particular test, default inversion:
-  'inversion' 'logdet' 'gradients'
+   'inversion' 'gradients'
 
 For all benchmarks, this constructs a variety of LMC kernels,
 all of which conform to the parameters n_o,d,q,eps specified
@@ -85,7 +85,7 @@ def _main():
     seed = int(sys.argv[7]) if len(sys.argv) > 7 else 1234
     testtype = sys.argv[8] if len(sys.argv) > 8 else 'inversion'
     kerntypes = ['rbf', 'periodic', 'matern', 'mix']
-    testtypes = ['inversion', 'logdet', 'gradients']
+    testtypes = ['inversion', 'gradients']
 
     assert n_o > 7
     assert d > 0
@@ -211,72 +211,6 @@ def run_kernel_benchmark(
     alpha_diff = np.fabs(apprx.deriv.alpha - exact.deriv.alpha).mean()
     print('        {:9.4e} |alpha_exact - alpha_apprx|_1 / n'
           .format(alpha_diff))
-
-    if testtype == 'logdet':
-        print('    logdet')
-        sgn, tru = np.linalg.slogdet(exact.K)
-        assert sgn > 0
-
-        # TODO: generalize logdet, abstract algorithms, use in optimization?
-        # TODO: in abstracted algorithms, need more robust 'cutoffs':
-        #       e.g., when convergence stalls
-
-        def minres(mvm):
-            ctr = 0
-            def cb(_):
-                nonlocal ctr
-                ctr += 1
-            m = len(grid_dists)
-            n = params.n
-            lo = sla.LinearOperator((n, n), matvec=mvm)
-
-            def inv_mvm(x):
-                Kinv_x, succ = sla.minres(
-                    lo, x, tol=1e-10, maxiter=m, callback=cb)
-                error = np.linalg.norm(x - mvm(Kinv_x))
-                #print('            '
-                #      'minres conv recon {:8.4e} it {:6d} m {:6d} succ {}'
-                #      .format(error, ctr, m, succ))
-                return Kinv_x
-
-            return inv_mvm
-
-        dd = np.diag(exact.K) # actual constructor harder w/o exact
-        D = lambda x: dd * x
-        N = lambda x: apprx.K.matvec(x) - D(x)
-        delta0 = np.log(dd).sum()
-
-        def mvm(t):
-            ndtn = lambda x: D(x) + t * N(x)
-            invmvm = minres(ndtn)
-            return lambda x: N(invmvm(x))
-
-        def apx_tr(mvm, n):
-            rs = np.random.randint(0, 2, (n, params.n)) * 2 - 1
-            trace = 0
-            var = 0
-            for r in rs:
-                x = r.dot(mvm(r))
-                trace += x
-            trace /= len(rs)
-            return trace
-
-        def sample(t):
-            return apx_tr(mvm(t), 2)
-
-        vs = np.vectorize(sample)
-
-        import scipy
-        integ = scipy.integrate.fixed_quad(vs, 0, 1, n=10)[0]
-        #integ, _, _, _ = scipy.integrate.quad(
-        # sample, 0, 1, limit=1, full_output=1)
-        trace = integ + delta0
-
-        print('        true value from exact {:8.4e}'.format(tru))
-        chol = np.log(np.diag(exact.L[0])).sum() * 2
-        print('        from cholesky diag    {:8.4e}'.format(chol))
-        print('        from         trace    {:8.4e}'.format(trace))
-        return
 
     def check_grads(f, name):
         with contexttimer.Timer() as t:
