527f1d422bbe822f4be9c29dcc04503bd1a2ba30
Vladimir Feinberg <vyf@princeton.edu>
2017-02-08 19:13:45 -0500

    script for f33k bench

--- runlmc/models/optimization.py
+++ runlmc/models/optimization.py
@@ -29,12 +29,16 @@ class MovingAverage:
 # TODO(test)
 class AdaDelta(Optimizer):
 
+    @staticmethod
+    def noop():
+        pass
+
     def __init__(self, **kwargs):
         super().__init__()
         default = {
             'step_rate':1, 'decay':0.9, 'momentum':0.5, 'offset':1e-4,
             'max_it':100, 'verbosity':0, 'min_grad_ratio':0.5, 'roll':1,
-            'permitted_drops':None}
+            'permitted_drops':None, 'callback':AdaDelta.noop}
         default.update(**kwargs)
         if default['permitted_drops'] is None:
             default['permitted_drops'] = max(default['max_it'] // 20, 1)
@@ -42,11 +46,11 @@ class AdaDelta(Optimizer):
 
     def opt(self, x, f_fp=None, f=None, fp=None):
         exclude = ['verbosity', 'min_grad_ratio', 'max_it', 'roll',
-                   'permitted_drops']
+                   'permitted_drops', 'callback']
         ada_kwargs = {k:v for k, v in self.kwargs.items() if k not in exclude}
         ada = climin.Adadelta(x, fp, **ada_kwargs)
 
-        verbosity, min_grad_ratio, max_it, roll, permitted_drops = (
+        verbosity, min_grad_ratio, max_it, roll, permitted_drops, callback = (
             self.kwargs[x] for x in exclude)
         delta = max(max_it // verbosity, 1) if verbosity else 0
         rolling_max = 1e-10
@@ -64,6 +68,8 @@ class AdaDelta(Optimizer):
             if verbosity and info['n_iter'] % delta == 0:
                 print('iteration {:8d}'.format(info['n_iter']),
                       'grad norm {:10.4e}'.format(gn))
+
+            callback()
 
             if ave < min_grad_ratio * rolling_max:
                 permitted_drops -= 1
