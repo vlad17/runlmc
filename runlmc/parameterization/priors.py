e35eed17e5c81b84a043a271b404539e6d860747
Vladimir Feinberg <vladf@berkeley.edu>
2017-05-27 20:01:19 -0400

    Added priors

--- runlmc/parameterization/priors.py
+++ runlmc/parameterization/priors.py
@@ -11,7 +11,9 @@
 This modules contains :class:`Prior`, the base type for all priors available.
 """
 
+import numpy as np
 from paramz.domains import _REAL
+from scipy.special import gammaln, digamma
 
 def _assert_no_constraints(x):
     assert all(c is _REAL for c in x._all_constraints())
@@ -58,3 +60,87 @@ class Prior:
         :return: the gradient of the log density of the prior at (each) `x`
         """
         raise NotImplementedError
+
+# TODO(cleanup): test and document below
+
+class Gaussian(Prior):
+    domain = 'real'
+
+    def __init__(self, mu, var):
+        if var <= 0:
+            raise ValueError('variance {} should be positive'.format(var))
+        self.mu = mu
+        self.var = var
+        self.constant = -0.5 * np.log(2 * np.pi * self.var)
+
+    def __str__(self):
+        return "N({:.2g}, {:.2g})".format(self.mu, self.var)
+
+    def lnpdf(self, x):
+        return self.constant - 0.5 * np.square(x - self.mu) / self.var
+
+    def lnpdf_grad(self, x):
+        return -(x - self.mu) / self.var
+
+class Gamma(Prior):
+    domain = 'positive'
+
+    def __init__(self, a, b):
+        self.a = float(a)
+        self.b = float(b)
+        self.constant = -gammaln(self.a) + a * np.log(b)
+
+    def __str__(self):
+        return "Gamma({:.2g}, {:.2g})".format(self.a, self.b)
+
+    def lnpdf(self, x):
+        return self.constant + (self.a - 1) * np.log(x) - self.b * x
+
+    def lnpdf_grad(self, x):
+        return (self.a - 1.) / x - self.b
+
+    @staticmethod
+    def from_EV(E, V):
+        """
+        Creates an instance of a Gamma Prior  by specifying the Expected value(s)
+        and Variance(s) of the distribution.
+
+        :param E: expected value
+        :param V: variance
+        """
+        a = np.square(E) / V
+        b = E / V
+        return Gamma(a, b)
+
+class InverseGamma(Prior):
+    domain = 'positive'
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.constant = -gammaln(self.a) + a * np.log(b)
+
+    def __str__(self):
+        return "InvGamma({:.2g}, {:.2g})".format(self.a, self.b)
+
+    def lnpdf(self, x):
+        return self.constant - (self.a + 1) * np.log(x) - self.b / x
+
+    def lnpdf_grad(self, x):
+        return -(self.a + 1.) / x + self.b / x ** 2
+
+class HalfLaplace:
+    domain = 'positive'
+
+    def __init__(self, b):
+        self.b = b
+        self.constant = - np.log(self.b)
+
+    def __str__(self):
+        return "HalfLaplace({:.2g})".format(self.b)
+
+    def lnpdf(self, x):
+        return self.constant - x / self.b
+
+    def lnpdf_grad(self, _):
+        return -1 / self.b
