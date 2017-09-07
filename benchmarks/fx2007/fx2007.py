21818632c59d10f586f0361b3a71552e6d544039
Vladimir Feinberg <vladf@berkeley.edu>
2017-09-07 14:18:09 -0700

    added asv code

--- benchmarks/fx2007/fx2007.py
+++ benchmarks/fx2007/fx2007.py
@@ -1,6 +1,9 @@
 # Compares on fx2007 dataset LLGP SLFM for varying interpolation points
 # vs COGP SLFM approx with fixed inducing points = 100
 
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+
 import numpy as np
 from standard_tester import *
 
@@ -19,18 +22,63 @@ else:
     interpolation_points = None
     inducing_points = 100
 
-xss, yss, test_xss, test_yss, _, _ = foreign_exchange_2007()
 
-np.random.seed(1234)
-kgen, rgen, slfmgen, indepgen = alvarez_and_lawrence_gp()
-llgp_stats = bench_runlmc(
-    runs, interpolation_points, xss, yss, test_xss, test_yss, kgen, rgen,
-    slfmgen, indepgen, {'verbosity': 100, 'min_grad_ratio': 0.2})
-print('---> llgp Q1R2 m', interpolation_points, statsline(llgp_stats))
+class Suite:
+    def setup_cache(self):
+        xss, yss, test_xss, test_yss, _, _ = foreign_exchange_2007()
 
-cogp_stats, _, _ = cogp_fx2007(cogp_runs, inducing_points)
-print('---> cogp m', inducing_points, statsline(cogp_stats))
+        np.random.seed(1234)
+        kgen, rgen, slfmgen, indepgen = alvarez_and_lawrence_gp()
+        llgp_stats = bench_runlmc(
+            runs, interpolation_points, xss, yss, test_xss, test_yss, kgen,
+            rgen, slfmgen, indepgen, {'verbosity': 100, 'min_grad_ratio': 0.2})
+        return llgp_stats
 
-latex_table('results_fx2007.tex',
-            ['LLGP', 'COGP'],
-            [llgp_stats, cogp_stats])
+    def track_mean_time(self, llgp_stats):
+        (mean_time, _), _, _ = llgp_stats
+        return mean_time
+
+    def track_se_time(self, llgp_stats):
+        (_, se_time), _, _ = llgp_stats
+        return se_time
+
+    def track_mean_smse(self, llgp_stats):
+        _, (mean_smse, _), _ = llgp_stats
+        return mean_smse
+
+    def track_se_smse(self, llgp_stats):
+        _, (_, se_smse), _ = llgp_stats
+        return se_smse
+
+    def track_mean_nlpd(self, llgp_stats):
+        _, _, (mean_nlpd, _) = llgp_stats
+        return mean_nlpd
+
+    def track_se_nlpd(self, llgp_stats):
+        _, _, (_, se_nlpd) = llgp_stats
+        return se_nlpd
+
+
+Suite.setup_cache.timeout = 3600 * 12
+Suite.track_mean_time.benchmark_name = 'fx2007 time mean'
+Suite.track_mean_time.units = 'seconds'
+Suite.track_se_time.benchmark_name = 'fx2007 time standard error'
+Suite.track_se_time.units = 'seconds'
+Suite.track_mean_smse.benchmark_name = 'fx2007 smse mean'
+Suite.track_se_smse.benchmark_name = 'fx2007 smse standard error'
+Suite.track_mean_nlpd.benchmark_name = 'fx2007 nlpd mean'
+Suite.track_se_nlpd.benchmark_name = 'fx2007 nlpd standard error'
+
+
+def main():
+    llgp_stats = Suite().setup_cache()
+    print('---> llgp Q1R2 m', interpolation_points, statsline(llgp_stats))
+    cogp_stats, _, _ = cogp_fx2007(cogp_runs, inducing_points)
+    print('---> cogp m', inducing_points, statsline(cogp_stats))
+    latex_table('results_fx2007.tex',
+                ['LLGP', 'COGP'],
+                [llgp_stats, cogp_stats])
+
+
+if __name__ == '__main__':
+    main()
--- benchmarks/weather/weather.py
+++ benchmarks/weather/weather.py
@@ -1,9 +1,10 @@
 # compares on 15K weather dataset LLGP SLFM reduction vs COGP SLFM approx
 
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+
 import numpy as np
 from standard_tester import *
-
-xss, yss, test_xss, test_yss, _ = weather()
 
 activate_logs()
 
@@ -20,26 +21,75 @@ else:
     interpolating_points = [500, 600, 700, 800, 900, 1000]
     inducing_points = 200
 
-llgp_stats = []
-for num_interp in interpolating_points:
-    np.random.seed(1234)
-    # rank 2 SLFM, same as Q=2 model of COGP
-    kgen, rgen, slfmgen, indepgen = slfm_gp(len(xss), 2)
-    stats = bench_runlmc(
-        runs, num_interp, xss, yss, test_xss, test_yss, kgen, rgen,
-        slfmgen, indepgen, {'verbosity': 100})
-    print('---> llgp slfm m', num_interp, statsline(stats))
-    llgp_stats.append(stats)
 
-cogp_stats, _, _ = cogp_weather(cogp_runs, inducing_points)
-print('---> cogp m', inducing_points, statsline(cogp_stats))
+class Suite:
+    def __init__(self, num_interp=750):
+        self.num_interp = 750
+
+    def setup_cache(self):
+        xss, yss, test_xss, test_yss, _ = weather()
+
+        np.random.seed(1234)
+        # rank 2 SLFM, same as Q=2 model of COGP
+        kgen, rgen, slfmgen, indepgen = slfm_gp(len(xss), 2)
+        llgp_stats = bench_runlmc(
+            runs, self.num_interp, xss, yss, test_xss, test_yss, kgen, rgen,
+            slfmgen, indepgen, {'verbosity': 100})
+        return llgp_stats
+
+    def track_mean_time(self, llgp_stats):
+        (mean_time, _), _, _ = llgp_stats
+        return mean_time
+
+    def track_se_time(self, llgp_stats):
+        (_, se_time), _, _ = llgp_stats
+        return se_time
+
+    def track_mean_smse(self, llgp_stats):
+        _, (mean_smse, _), _ = llgp_stats
+        return mean_smse
+
+    def track_se_smse(self, llgp_stats):
+        _, (_, se_smse), _ = llgp_stats
+        return se_smse
+
+    def track_mean_nlpd(self, llgp_stats):
+        _, _, (mean_nlpd, _) = llgp_stats
+        return mean_nlpd
+
+    def track_se_nlpd(self, llgp_stats):
+        _, _, (_, se_nlpd) = llgp_stats
+        return se_nlpd
+
+
+Suite.setup_cache.timeout = 3600 * 12
+Suite.track_mean_time.benchmark_name = 'weather time mean'
+Suite.track_mean_time.units = 'seconds'
+Suite.track_se_time.benchmark_name = 'weather time standard error'
+Suite.track_se_time.units = 'seconds'
+Suite.track_mean_smse.benchmark_name = 'weather smse mean'
+Suite.track_se_smse.benchmark_name = 'weather smse standard error'
+Suite.track_mean_nlpd.benchmark_name = 'weather nlpd mean'
+Suite.track_se_nlpd.benchmark_name = 'weather nlpd standard error'
+
 
 def make_llgp_colname(interp):
     return r'\begin{tabular}{c}LLGP\\$m=' + str(interp) + r'$\end{tabular}'
 
-colnames = [make_llgp_colname(interpolating_points[0]),
-            make_llgp_colname(interpolating_points[-1]),
-            'COGP']
-latex_table('results_weather.tex',
-            colnames,
-            [llgp_stats[0], llgp_stats[-1], cogp_stats])
+
+def main():
+    llgp_stats = []
+    for num_interp in interpolating_points:
+        stats = Suite(num_interp).setup_cache()
+        llgp_stats.append(stats)
+        print('---> llgp slfm m', num_interp, statsline(stats))
+
+    cogp_stats, _, _ = cogp_weather(cogp_runs, inducing_points)
+    print('---> cogp m', inducing_points, statsline(cogp_stats))
+
+    colnames = [make_llgp_colname(interpolating_points[0]),
+                make_llgp_colname(interpolating_points[-1]),
+                'COGP']
+    latex_table('results_weather.tex',
+                colnames,
+                [llgp_stats[0], llgp_stats[-1], cogp_stats])
