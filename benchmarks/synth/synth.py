db93bb96437c032d407b7dc7f09c92c2b486faba
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2017-10-11 08:34:50 -0700

    cogp+

--- benchmarks/synth/synth.py
+++ benchmarks/synth/synth.py
@@ -28,12 +28,13 @@ def main():
     activate_logs()
 
     llgp_runs = 3
-    cogp_runs = 3
-    cogp_max_it = [1000]
+    cogp_runs = 1
+    cogp_max_it = [100, 100]
     interpolating_points = [25, 25]
     nthreads = 16
-    inducing_points = [500]
-    nbatches = [1000]
+    inducing_points = [500, 5000]
+    nbatches = [1000, 1000]
+    cogp_colnames = ['COGP', 'COGP+']
 
     all_stats = []
     colnames = []
@@ -44,6 +45,7 @@ def main():
         nbatches = [int(sys.argv[4])]
         cogp_max_it = [int(sys.argv[5])]
         cogp_runs = 1
+        cogp_colnames = ['COGP']
     else:
         kgen, rgen, slfmgen, indepgen = synth_gen()
         xss, yss, test_xss, test_yss = synth()
@@ -55,15 +57,13 @@ def main():
         all_stats = [stats]
         colnames = ['LLGP']
 
-    for ind, nb, mi in zip(inducing_points, nbatches, cogp_max_it):
+    for ind, nb, mi, name in zip(inducing_points, nbatches, cogp_max_it, cogp_colnames):
         cogp_stats = cogp_synth(
             cogp_runs, ind, nthreads, nb, mi)
         dump(cogp_stats, 'cogp_stats-{}-{}'.format(ind, nb))
         # cogp_stats = load('cogp_stats-{}-{}'.format(ind, nb))
         all_stats.append(cogp_stats)
-        colnames.append(r'\begin{tabular}{c}COGP\\$m=' +
-                        str(ind) + r'$\\ $n_b=' + str(nb) +
-                        r'$\end{tabular}')
+        colnames.append(name)
 
     latex_table('results_synth.tex', colnames, all_stats)
 
