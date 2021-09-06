4d57e951c3565f05d43bf1c12e59b79eabcd51bf
Vladimir Feinberg <vladf@berkeley.edu>
2021-09-05 17:07:41 -0700

    final template gen

--- arxiv-tar.sh
+++ arxiv-tar.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+mkdir -p paper/anc
+cp paper/supplement.pdf paper/anc
 tar --exclude="paper/*.eps" --exclude="paper/.gitignore" --exclude="*.sh" --exclude="paper/*.out" --exclude="paper/supplement*" --exclude="paper/paper.pdf" -czvf paper.tgz paper/
+rm -rf paper/anc
